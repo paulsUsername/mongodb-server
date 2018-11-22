8d33bf875edd862bf4f32fbefa8b03ea8c7c95b3
admin <admin@MacBooks-MacBook-Pro.local>
2018-11-22 20:31:37 +0000

    Added more new mongodb functions

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -13,6 +13,16 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true 
     var db = client.db('TodoApp');
 
 
+    db.collection('Users').findOneAndUpdate({
+        name: "Mark Finlay"
+    },{$inc : {'age': 1}}, {returnOriginal: false}).then((result) => {
+        console.log(result);
+        client.close();
+    }, (rejected) => {
+        console.log("Update to value was rejected", rejected);
+    });
+
+    //**********************Find via Object ID
     // db.collection('ToDos').find({
     //     _id: new ObjectID('5bf6ea341cc4efb5945784ee')
     // })
@@ -24,6 +34,7 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true 
     //     }
     // });
 
+    //**********************Get count of total documents */
     // db.collection('ToDos').find().count().then((count) => {
     //     console.log(`ToDos: count ${count}`)
     // }, (err) => {
@@ -32,18 +43,32 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true 
     //     }
     // });
 
-    db.collection('Users').find({
-        name: "Paul Finlay"
-    }).toArray().then((docs) => {
-        console.log(JSON.stringify(docs, undefined, 4));
-        if (docs) {
-            client.close();
-        }
-    }, (err) => {
-        if (err) {
-            console.log(`Error getting users from database with error: ${err}`)
-        }
-    });
+    //**********************Find User with parameter */
+    // db.collection('Users').find({
+    //     name: "Paul Finlay"
+    // }).toArray().then((docs) => {
+    //     console.log(JSON.stringify(docs, undefined, 4));
+    //     if (docs) {
+    //         client.close();
+    //     }
+    // }, (err) => {
+    //     if (err) {
+    //         console.log(`Error getting users from database with error: ${err}`)
+    //     }
+    // });
+
+
+    //****************DELETE */
+    // db.collection('Users').deleteOne({
+    //     name: "Colm Finlay"
+    // }).then((result) => {
+    //     console.log(result.deletedCount)
+    //     client.close();
+    // }, (rejected) => {
+    //     console.log(rejected)
+    // });
+
+
 
 
 //     client.close();
